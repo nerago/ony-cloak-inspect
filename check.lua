2fe0f0730d73b89a29a94a2fbd58fb9c358bd4c0
Nicholas Hardy <nicholas.h.hardy@gmail.com>
2024-10-12 14:54:02 +1100

    r

--- check.lua
+++ check.lua
@@ -7,34 +7,39 @@ function addon:onEvent(_, event, ...)
 	local arg1, arg2 = select(1, ...), select(2, ...);
     if event == "ADDON_LOADED" then
         if arg1 == "NeragoCheckOnyCloak" then
-            addon:infoMessage("Loaded");
-			SlashCmdList["NERAGO_ONY_CHECK"] = function(...) addon:slashCommand(...) end;
+            self:infoMessage("Loaded");
+			SlashCmdList["NERAGO_ONY_CHECK"] = function(...) self:slashCommand(...) end;
 			SLASH_NERAGO_ONY_CHECK1 = "/ony";
 			if not NERAGO_ONY_CHECK_SAVED then
 				NERAGO_ONY_CHECK_SAVED = {};
 			end
-			addon.loaded = true;
-			addon.checking = false;
+			self.loaded = true;
+			self.checking = false;
         end
-	elseif addon.loaded then
+	elseif self.loaded then
 		if event == "GROUP_ROSTER_UPDATE" then
 			--todo
+		elseif event == "PLAYER_REGEN_DISABLED" then
+			self.checking = false;
+			if self.dialog then
+				self.dialog:Hide();
+			end
 		elseif event == "PLAYER_TARGET_CHANGED" then
 			self:checkTarget();
 		elseif event == "INSPECT_READY" then
-			addon:inspectReady(arg1);
+			self:inspectReady(arg1);
 		end
     end
 end
 
 function addon:slashCommand(...)
-	self:createFrame();
+	--self:createFrame();
+	self:createButton00();
 	--self:startCheck();
 end
 
-function addon:startCheck()
-	local numMembers = GetNumGroupMembers(LE_PARTY_CATEGORY_HOME);
-	self.status = {};
+function addon:listGroupMembers()
+	local members = {};
 	
 	if IsInRaid() then
 		for i = 1, MAX_RAID_MEMBERS do
@@ -42,8 +47,7 @@ function addon:startCheck()
 			local unitId = "raid" .. i;
 			local name, guid = UnitNameUnmodified(unitId), UnitGUID(unitId);
 			if name and guid then
-				-- GetPlayerInfoByGUID
-				self.status[guid] = { guid = guid, name = name };
+				tinsert(members, { guid = guid, name = name });
 			end
 		end
 	elseif IsInGroup() then
@@ -51,15 +55,25 @@ function addon:startCheck()
 			local unitId = "party" .. i;
 			local name, guid = UnitNameUnmodified(unitId), UnitGUID(unitId);
 			if name and guid then
-				self.status[guid] = { guid = guid, name = name };
+				tinsert(members, { guid = guid, name = name });
 			end
 		end
 	else
-		infoMessage("Not in group")
+		local unitId = "player";
+		local name, guid = UnitNameUnmodified(unitId), UnitGUID(unitId);
+		tinsert(members, { guid = guid, name = name });
+		infoMessage("Not in group");
 	end
-	
+	return members;
+end
+
+function addon:startCheck()
+	local members = self:listGroupMembers();
+	self.status = {};
 	self.dataProvider.RemoveIndexRange(1, self.dataProvider:GetSize());
-	for guid, tab in pairs(self.status) do
+	for _, tab in ipairs(members) do
+		tab.cloak = "?";
+		self.status[tab.guid] = tab;
 		self.dataProvider:Insert(tab)
 	end
 	
@@ -88,12 +102,17 @@ function addon:initRow(frame, data)
 	if not frame.left then
 		local buttonName = "NeragoCheckOnyCloakCheckButton"..nameIndex;
 		nameIndex = nameIndex + 1
-		frame.button = CreateFrame("Button", buttonName, frame, "UIPanelButtonTemplate,InsecureActionButtonTemplate")
+		--frame.button = CreateFrame("Button", buttonName, frame, "InsecureActionButtonTemplate,UIPanelButtonTemplate")
+		frame.button = CreateFrame("Button", buttonName, UIParent, "InsecureActionButtonTemplate,UIPanelButtonTemplate")
 		
 		frame.button:SetPoint("TOPRIGHT", frame, "TOPRIGHT")
 		frame.button:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT")
 		frame.button:SetText("Check")
 		frame.button:SetSize(50, 20)
+		
+		frame.button:SetScript("PostClick", function(_, button, down)
+			print(frame.button:GetAttribute("type") .. " " .. frame.button:GetAttribute("target"))
+		end)
 		
 		frame.left = frame:CreateFontString(nil, "OVERLAY", "GameTooltipText")
 		frame.left:SetPoint("LEFT", frame)
@@ -104,8 +123,15 @@ function addon:initRow(frame, data)
 	end
 	
 	frame.left:SetText(data.name)
-	frame.button:SetAttribute("type", "target")
-	frame.button:SetAttribute("target", data.name)
+	if not InCombatLockdown() then
+		frame.button:SetAttribute("type", "target")
+		frame.button:SetAttribute("target", data.name)
+		--btn:SetAttribute("type", "macro")
+        --btn:SetAttribute("macrotext", "/target Neravi")
+		frame.button:Show()
+	else
+		frame.button:Hide()
+	end
 	--frame.right:SetText("123")
 	--frame.right:Hide()
 	--frame.button:Show()
@@ -117,7 +143,7 @@ function addon:pressPlayer(data)
 end
 
 function addon:createFrame()
-	local dialog = self.frame;
+	local dialog = self.dialog;
 	if not dialog then
 		dialog = CreateFrame("Frame", "NeragoCheckOnyFrame", UIParent, "BasicFrameTemplateWithInset");
 		dialog.TitleText:SetText("Check Onyixa Cloak")
@@ -172,14 +198,15 @@ function addon:createFrame()
 		scrollBar:SetPoint("BOTTOMLEFT", scrollBox, "BOTTOMRIGHT", 6, 0)
 		
 		local dataProvider = CreateDataProvider()
-		dataProvider:SetSortComparator(function(a, b) if a.name < b.name then return -1 elseif a.name > b.name then return 1 else return 0 end end);
+		-- dataProvider:SetSortComparator(function(a, b) if a.name < b.name then return -1 elseif a.name > b.name then return 1 else return 0 end end);
 		local scrollView = CreateScrollBoxListLinearView()
 		scrollView:SetDataProvider(dataProvider)
 		scrollView:SetElementExtent(20)
 		scrollView:SetElementInitializer("Frame", function(a, b) self:initRow(a, b) end)
 		ScrollUtil.InitScrollBoxListWithScrollBar(scrollBox, scrollBar, scrollView)
 		
-		dataProvider:Insert({name="Nerago"},{name="Average"},{name="Gornek"})
+		-- dataProvider:Insert({name="Nerago"},{name="Average"},{name="Gornek"},{name="Neravi"})
+		dataProvider:Insert({name="Nerago"},{name="Average"},{name="Gornek"},{name="player"})
 		
 		self.dataProvider = dataProvider;
 		self.dialog = dialog;
@@ -188,13 +215,42 @@ function addon:createFrame()
 	--self:updateButton();
 end
 
+function addon:createButton00()
+	local btn = self.button;
+	if not btn then
+		btn = CreateFrame("Button", "NeragoOnyCheekButton", UIParent, "SecureActionButtonTemplate")
+		btn:SetSize(50, 50);
+		btn:SetAttribute("type", "target");
+		btn:SetAttribute("target", "Neravi")
+		--btn:SetAttribute("type", "macro")
+        --btn:SetAttribute("macrotext", "/target Neravi")
+		
+		btn:SetPoint("CENTER");
+		
+		btn.tex = btn:CreateTexture()
+		btn.tex:SetAllPoints(btn)
+		btn.tex:SetTexture("interface/icons/inv_misc_questionmark")
+		
+		btn:RegisterForClicks("AnyDown");
+		btn:SetScript("PostClick", function(_, button, down)
+			self:infoMessage("feed");
+		end)
+			
+		btn:SetMovable(true);
+		btn:RegisterForDrag("RightButton");
+		
+		self.button = btn;
+	end
+	
+end
+
 function addon:infoMessage(message)
 	print("|cFFE533E5[Check Onyixa Cloak]|r "..tostring(message));
 end
 
 local events = CreateFrame("Frame");
 events:RegisterEvent("ADDON_LOADED");
-events:RegisterEvent("GROUP_ROSTER_UPDATE");
+events:RegisterEvent("PLAYER_REGEN_DISABLED");
 events:RegisterEvent("PLAYER_TARGET_CHANGED");
 events:RegisterEvent("INSPECT_READY");
 events:SetScript("OnEvent", function(...) addon:onEvent(...) end);
